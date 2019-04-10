c42d3d7a84461d1843a5f01cb4ed07ce853db3c9
Justin <justin@thatdevjustin.com>
2019-04-09 20:51:56 -0400

    standard grade book added and if statement for adding that type

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
