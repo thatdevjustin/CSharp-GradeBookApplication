2ad559cb6e81f490a10c2934d6a4d95425b3f7eb
Justin <justin@thatdevjustin.com>
2019-04-09 20:41:23 -0400

    return if failed

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -35,7 +35,11 @@ namespace GradeBook.GradeBooks
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
+            {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
 
             base.CalculateStatistics();
         }
@@ -43,7 +47,10 @@ namespace GradeBook.GradeBooks
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
+            {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
 
             base.CalculateStudentStatistics(name);
         }
